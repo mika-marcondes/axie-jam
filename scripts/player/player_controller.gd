dca5390eca3072e66f67ba94707dabc38cf41cd6
Mikael <mika.marcondes2@gmail.com>
2026-09-15 18:16:18 -0300

    rotate puffy on air

--- scripts/player/player_controller.gd
+++ scripts/player/player_controller.gd
@@ -1,9 +1,18 @@
 extends Node3D
 
+@export_category("References")
 @export var ball: BallController
+
+@export_category("Movement")
 @export var rotation_speed: float = 10.0
 
-@onready var visual_root: Node3D = $VisualRoot
+@export_category("Air Tricks")
+@export var air_spin_speed: float = 360.0
+@export var landing_realign_speed: float = 8.0
+
+@onready var trick_pivot: Node3D = $TrickPivot
+
+var air_spin_angle: float = 0.0
 
 
 func _physics_process(delta: float) -> void:
@@ -11,7 +20,12 @@ func _physics_process(delta: float) -> void:
 		return
 
 	follow_ball()
-	update_facing(delta)
+
+	if ball.is_on_floor():
+		update_facing(delta)
+		realign_trick(delta)
+	else:
+		handle_air_spin(delta)
 
 
 func follow_ball() -> void:
@@ -34,3 +48,24 @@ func update_facing(delta: float) -> void:
 		target_yaw,
 		clampf(rotation_speed * delta, 0.0, 1.0)
 	)
+
+
+func handle_air_spin(delta: float) -> void:
+	var spin_input: float = Input.get_axis(
+		"trick_spin_left", "trick_spin_right"
+	)
+
+	air_spin_angle += spin_input * deg_to_rad(air_spin_speed) * delta
+	air_spin_angle = wrapf(air_spin_angle, -PI, PI)
+
+	trick_pivot.rotation.y = air_spin_angle
+
+
+func realign_trick(delta: float) -> void:
+	air_spin_angle = lerp_angle(
+		air_spin_angle,
+		0.0,
+		clampf(landing_realign_speed * delta, 0.0, 1.0)
+	)
+
+	trick_pivot.rotation.y = air_spin_angle
