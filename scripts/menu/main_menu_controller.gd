b0ce0779b61c4c888f2e0d979c6ff36ee54ba8b3
Mikael <mika.marcondes2@gmail.com>
2026-09-20 22:59:48 -0300

    basic menu done

---
+++ scripts/menu/main_menu_controller.gd
@@ -0,0 +1,68 @@
+extends Node3D
+class_name MainMenuController
+
+@export_file("*.tscn")
+var contest_scene_path: String = (
+	"res://scenes/contest/contest_greybox.tscn"
+)
+
+@onready var axie_name_label: Label = %AxieNameLabel
+@onready var next_axie_button: Button = %NextAxieButton
+@onready var play_button: Button = %PlayButton
+
+@export_category("References")
+@export var camera: Camera3D
+@export var name_tag_anchor: Marker3D
+
+@onready var character_tag: Control = %CharacterTag
+
+func _ready() -> void:
+	axie_name_label.text = "PUFFY"
+
+	play_button.pressed.connect(
+		_on_play_pressed
+	)
+
+	next_axie_button.pressed.connect(
+		_on_next_axie_pressed
+	)
+
+	play_button.grab_focus()
+
+
+func _process(_delta: float) -> void:
+	update_character_tag_position()
+
+
+func update_character_tag_position() -> void:
+	if camera == null or name_tag_anchor == null:
+		return
+
+	if camera.is_position_behind(
+		name_tag_anchor.global_position
+	):
+		character_tag.visible = false
+		return
+
+	character_tag.visible = true
+
+	var screen_position: Vector2 = (
+		camera.unproject_position(
+			name_tag_anchor.global_position
+		)
+	)
+
+	character_tag.position = (
+		screen_position
+		- character_tag.size * 0.5
+	)
+
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file(
+		contest_scene_path
+	)
+
+
+func _on_next_axie_pressed() -> void:
+	print("Character swap coming next.")
